458deb900da027c87d901498cc59d653b764c7b6
Samay28 <imsamaystar@gmail.com>
2023-11-05 20:23:11 +0530

    delegates

--- Source/MultiplayerCoop/Private/MultiplayerSessionsSubsystem.cpp
+++ Source/MultiplayerCoop/Private/MultiplayerSessionsSubsystem.cpp
@@ -2,6 +2,7 @@
 
 #include "MultiplayerSessionsSubsystem.h"
 #include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h" //for creating sessions
 
 void PrintString(const FString &str)
 {
@@ -26,7 +27,7 @@ void UMultiplayerSessionsSubsystem::Initialize(FSubsystemCollectionBase &Collect
 
         if (SessionInterface.IsValid())
         {
-            PrintString("Valid hai bhai");
+            SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UMultiplayerSessionsSubsystem::OnCreateSessionComplete);
         }
     }
 }
@@ -36,10 +37,43 @@ void UMultiplayerSessionsSubsystem::Deinitialize()
 
 void UMultiplayerSessionsSubsystem::CreateServer(FString ServerName)
 {
-    PrintString("Created");
+
+    // if (ServerName.IsEmpty())
+    // {
+    //     PrintString("Server Name Cannot Be Empty");
+    //     return;
+    // }
+    FName MySessionName = FName("Co-op Adventure Session Name");
+    FOnlineSessionSettings SessionSettings;
+    SessionSettings.bAllowJoinInProgress = true;
+    SessionSettings.bIsDedicated = false;
+    SessionSettings.bShouldAdvertise = true;
+    SessionSettings.NumPublicConnections = 2;
+    SessionSettings.bUseLobbiesIfAvailable = true;
+    SessionSettings.bUsesPresence = true;
+    SessionSettings.bAllowJoinViaPresence = true;
+    bool IsLan = false;
+
+    if (IOnlineSubsystem::Get()->GetSubsystemName() == "NULL")
+    {
+        IsLan = true;
+    }
+    SessionSettings.bIsLANMatch = IsLan;
+
+    SessionInterface->CreateSession(0, MySessionName, SessionSettings);
 }
 
 void UMultiplayerSessionsSubsystem::FindServer(FString ServerName)
 {
     PrintString("Joined Server");
+}
+
+void UMultiplayerSessionsSubsystem::OnCreateSessionComplete(FName SessionName, bool WasSuccessful)
+{
+    PrintString(FString::Printf(TEXT("%d"), WasSuccessful));
+
+    if(WasSuccessful)
+    {
+        GetWorld()->ServerTravel("/Game/ThirdPerson/Maps/ThirdPersonMap?listen"); //launch map as a listen server
+    }
 }
