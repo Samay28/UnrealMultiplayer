ebf2a23c1965ab88bce0a9b2e831a0f8efdae8de
Samay28 <imsamaystar@gmail.com>
2023-12-31 15:55:04 +0530

    done

---
+++ Source/MultiplayerCoop/Private/WinArea.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WinArea.h"
+
+// Sets default values
+AWinArea::AWinArea()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AWinArea::MulticastRPCWin_Implementation()
+{
+	OnWinCondition.Broadcast();
+}
+
+// Called when the game starts or when spawned
+void AWinArea::BeginPlay()
+{
+	Super::BeginPlay();
+	MulticastRPCWin();
+}
+
+// Called every frame
+void AWinArea::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
